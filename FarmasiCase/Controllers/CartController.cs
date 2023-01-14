f316d0e82d3b8434a842211a4c0de203bc1d0179
Yasemin-agman <agmanyaseminn@gmail.com>
2023-01-14 11:19:49 +0300

    Cart Controller sayfasında düzenlemeler yapılarak sepete ekleme yapıldı.

--- FarmasiCase/Controllers/CartController.cs
+++ FarmasiCase/Controllers/CartController.cs
@@ -4,29 +4,53 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using MongoDB.Bson;
 using MongoDB.Driver;
+using System.Security.Cryptography;
 
 namespace FarmasiCase.Controllers
 {
     public class CartController : Controller
     {
-        private object db;
-        private readonly IWebHostEnvironment env;
-        IMongoClient mongoClient = new MongoClient("mongodb://localhost:27017");
-        private object urunler;
-
-        public object Session { get; private set; }
+        private MongoClient _mongoClient = null;
+        private IMongoDatabase _database = null;
+        private IMongoCollection<Cart> carttablo = null;
+        private IMongoCollection<Urunler> uruntablo = null;
+        
+        public CartController()
+        {
+            _mongoClient = new MongoClient("mongodb://localhost:27017");
+            _database = _mongoClient.GetDatabase("Farmasi");
+            uruntablo = _database.GetCollection<Urunler>("urunler");
+            carttablo = _database.GetCollection<Cart>("Cart");
+        }
 
         public IActionResult Index()
-        {
-            
-            return View();
+        {                  
+            var cartResults = carttablo.Find<Cart>(a => true).ToList();
+            return View(cartResults);
         }
-        public IActionResult AddToCart(int Id)
-        {
-           
 
-            return View();
+
+        public IActionResult AddToCart(string id)
+        {
+
+            var urun = uruntablo.Find(c => c.Id == id).FirstOrDefault();
+            Cart newcart = new Cart();
+            newcart.Adet = 1;
+            newcart.Urunler = urun;        
+            carttablo.InsertOne(newcart);
+            return RedirectToAction("Index");
+           //return View(urunResults);
+
         }
-        
+        public IActionResult Delete(string id)
+        {
+            var ara = Builders<Cart>.Filter.Eq(c => c._id, id);
+            carttablo.DeleteOne(ara);
+            return RedirectToAction("Index");
+            //return View(urunResults);
+
+        }
+
+
     }
 }
--- FarmasiCase/Models/Cart.cs
+++ FarmasiCase/Models/Cart.cs
@@ -1,41 +1,50 @@
-﻿namespace FarmasiCase.Models
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace FarmasiCase.Models
 {
     public class Cart
     {
-        private List<CartLine> _cartLines = new List<CartLine>();
-        public List<CartLine> CartLines
-        {
-            get { return _cartLines; }
-        }
-        public void AddUrunler(Urunler urun, int adet)
-        {
-            var line = _cartLines.FirstOrDefault(i => i.Urunler.Id == urun.Id);
-            if (line == null)
-            {
-                _cartLines.Add(new CartLine() { Urunler = urun, Adet = adet });
-            }
-            else
-            {
-                line.Adet += adet;
-            }
-        }
-        public void DeleteUrunler(Urunler urun)
-        {
-            _cartLines.RemoveAll(i => i.Urunler.Id == urun.Id);
-        }
-        public decimal Total()
-        {
-            return _cartLines.Sum(i => i.Urunler.Fiyat * i.Adet);
-        }
-        public void Clear()
-        {
-            _cartLines.Clear();
-        }
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string _id { get; set; }
+        public Urunler Urunler { get; set; }
+        public int Adet { get; set; }
+        
+        //private List<CartLine> _cartLines = new List<CartLine>();
+        //public List<CartLine> CartLines
+        //{
+        //    get { return _cartLines; }
+        //}
+        //public void AddUrunler(Urunler urun, int adet)
+        //{
+        //    var line = _cartLines.FirstOrDefault(i => i.Urunler.Id == urun.Id);
+        //    if (line == null)
+        //    {
+        //        _cartLines.Add(new CartLine() { Urunler = urun, Adet = adet });
+        //    }
+        //    else
+        //    {
+        //        line.Adet += adet;
+        //    }
+        //}
+        //public void DeleteUrunler(Urunler urun)
+        //{
+        //    _cartLines.RemoveAll(i => i.Urunler.Id == urun.Id);
+        //}
+        //public decimal Total()
+        //{
+        //    return _cartLines.Sum(i => i.Urunler.Fiyat * i.Adet);
+        //}
+        //public void Clear()
+        //{
+        //    _cartLines.Clear();
+        //}
 
-        internal void AddUrunler(object urunler)
-        {
-            throw new NotImplementedException();
-        }
+        //internal void AddUrunler(object urunler)
+        //{
+        //    throw new NotImplementedException();
+        //}
     }
     public class CartLine
     {
